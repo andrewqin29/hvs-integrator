1d1ef3e681e55192bfdf734ca22abdaced7d97b2
Andrew Qin <andrewqin29@gmail.com>
2025-07-02 01:55:08 -0700

    implement nfw_acceleration

--- src/hvs_orbital_kinematics/potentials.py
+++ src/hvs_orbital_kinematics/potentials.py
@@ -4,10 +4,7 @@ import astropy.units as u
 
 # express gravitational constant in terms of standard units (kpc, m_Sun, Myr)
 G_KPC_MYR = const.G.to(u.kpc**3 / (u.Msun * u.Myr**2)).value
-
-# define potential component functions which takes a 3D position vector and a dictionary of parameters
-# describing the galactic component and returns the 3D acceleration vector produced by that component
-# so in the end, we just sum up vectors from the 3 components: hernquist buldge, miyamoto nagai disks and NFW
+PI = 3.1415926535
 
 def hernquist_acceleration(pos, params):
     """
@@ -16,41 +13,98 @@ def hernquist_acceleration(pos, params):
 
     Parameters:
         - pos: The 3d position vector of the HVS
-        - params: A dictionary of the parameters. 'm' for mass in Suns, 'c' for scale radius (kpc)
+        - params: A dictionary of the parameters. 'm' for mass in Suns, 'a' for scale radius (kpc)
     """
     r = np.linalg.norm(pos) # radius from center
     if r == 0:
         return np.array([0., 0., 0.]) # early check preventing divide by 0
-    mass_term = G_KPC_MYR * params['m']
-    denominator = r * (r + params['c'])**2
+    
+    m, a = params['m'], params['a']
+    mass_term = G_KPC_MYR * m
+    denominator = r * (r + a)**2
 
-    return -mass_term / denominator * pos #directed towards galactic center
+    return -mass_term / denominator * pos # directed towards galactic center
 
 def miyamoto_nagai_acceleration(pos, params):
+    """
+    Calculates acceleration due to a Miyamoto-Nagai Disk. Will be used for all three disks in MN3 model.
+
+    Parameters:
+        - pos: The 3d position vector of the HVS
+        - params: A dictionary of the parameters. 'm' for mass in Suns, 'l' for scale length (kpc), 'h' for scale height
+    """
     pass
 
 def nfw_acceleration(pos, params):
-    pass
+    """
+    Calculates acceleration due to NFW dark matter halo potential.
+
+    Parameters:
+        - pos: The 3d position vector of the HVS
+        - params: A dictionary of the parameters. 'rho' for scale density, 'a' for scale radius (kpc)
+    """
+    r = np.linalg.norm(pos)
+    if r == 0:
+        return np.array([0., 0., 0.])
+    
+    rho = params['rho0']
+    a = params['a']
+    factored_term = -4 * PI * G_KPC_MYR * rho * (a**3) / (r**3)
+    bracket_term = r / (r + a) - np.log(1 + r / a)
+
+    return factored_term * bracket_term * pos
+
+
 
 class MWPotential:
     def __init__(self):
+
+        # define component values
+        rho0_halo = None #FIND THESE VALUES LATER
+        scale_radius_halo = None
+    
         self.components = {
             # initialize parameters for the components
-            'Nucleus': {
+            'nucleus': {
                 'function': hernquist_acceleration,
-                'params': {'m': 4.297e6 * u.Msun, 'c': 0.0001 * u.kpc}
+                'params': {
+                    'm': 4.297e6, # mass of SGR A* in sun mass
+                    'a': 0.00001 # is this right?? -- trying to treat as point mass
+                    }
             },
 
-            'HernquistBulge': {
-
+            'bulge': {
+                'function': hernquist_acceleration,
+                'params': {
+                    # NEED MASS AND SCALE RADIUS OF BULGE
+                }
             },
 
-            'MiyamotoNagaiDisks': {
-
+            'disk': {
+                'function': miyamoto_nagai_acceleration
             },
 
-            'NFWHalo': {
-
+            'halo': {
+                'function': nfw_acceleration,
+                'params': {
+                    'rho0': rho0_halo,
+                    'a': scale_radius_halo
+                    }
             }
 
         }
+    
+    def get_acceleration(self, pos):
+        '''
+        Calculates the total gravitational acceleration at a given position by summing contributions
+        from galactic components. Returns a 3D acceleration vector in kpc/Myr^2
+
+        Parameters:
+            - pos: a 3D np.array of the position vector [x,y,z] in kpc
+        '''
+        ret = np.array([0., 0., 0.])
+
+        for component in self.components.values():
+            ret += component['function'](pos, component['params'])
+        
+        return ret

ccb563d4810175b72855cf39cdd01cf4e912f53e
Andrew Qin <andrewqin29@gmail.com>
2025-07-17 12:08:09 -0700

    finish interpolation poc and begin package

--- src/hvs_orbital_kinematics/interpolators.py
+++ src/hvs_orbital_kinematics/interpolators.py
@@ -1,9 +1,4 @@
-# interpolation on LMC and MW trajectory data
-import os
-
-file_dir = os.getcwd()
-project_root = os.path.abspath(os.path.join(file_dir, os.pardir))
-mw_orbit_path = os.path.join(project_root, 'data', 'raw', 'trajectories', 'GC21M2b1_orbit_mw.txt')
-lmc_orbit_path = os.path.join(project_root, 'data', 'raw', 'trajectories', 'GC21M3b1_orbit_lmc.txt')
-
-
+import pandas as pd
+import numpy as np
+from scipy.optimize import curve_fit
+impot os

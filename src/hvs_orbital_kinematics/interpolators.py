f4983cf4010cd9593a95192b4b15b33362b17a87
Andrew Qin <andrewqin29@gmail.com>
2025-07-30 17:51:19 -0700

    add velocity interpolation

--- src/hvs_orbital_kinematics/interpolators.py
+++ src/hvs_orbital_kinematics/interpolators.py
@@ -4,19 +4,18 @@ import astropy.units as u
 
 class TrajectoryInterpolator:
     """
-    A class to load and linearly interpolate trajectory data for the MW and LMC
-    
-    The input data file is expected to be a whitespace-delimited text file with four
-    columns: time, x, y, and z, and no header.
+    A class to load and linearly interpolate trajectory data for the MW and LMC.
     """
     def __init__(self, file_path: str):
+        
         self.GYR_TO_MYR = 1000.0
+        self.KM_S_TO_KPC_MYR = (u.km / u.s).to(u.kpc / u.Myr).value
 
         self.file_path = file_path
         self.data = self._load_data()
 
-    def _load_data(self) -> pd.DataFrame:
-        col_names = ['time', 'x', 'y', 'z']
+    def _load_data(self) -> pd.DataFrame:    
+        col_names = ['time', 'x', 'y', 'z', 'vx', 'vy', 'vz']
         
         df = pd.read_csv(
             self.file_path,
@@ -24,9 +23,12 @@ class TrajectoryInterpolator:
             header=None,
             names=col_names
         )
-        #convert gyr to myr
+
         df['time'] *= self.GYR_TO_MYR
-        #ensure sorted
+        df['vx'] *= self.KM_S_TO_KPC_MYR
+        df['vy'] *= self.KM_S_TO_KPC_MYR
+        df['vz'] *= self.KM_S_TO_KPC_MYR
+        
         df = df.sort_values(by='time').reset_index(drop=True)
         
         return df
@@ -37,3 +39,10 @@ class TrajectoryInterpolator:
         z = np.interp(t, self.data['time'], self.data['z'])
         
         return np.array([x, y, z])
+
+    def get_velocity(self, t: float) -> np.ndarray:
+        vx = np.interp(t, self.data['time'], self.data['vx'])
+        vy = np.interp(t, self.data['time'], self.data['vy'])
+        vz = np.interp(t, self.data['time'], self.data['vz'])
+        
+        return np.array([vx, vy, vz])
